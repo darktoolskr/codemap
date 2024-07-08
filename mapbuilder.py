7b12cc7b010be5462cee9de87c804eefbe9d1d97
darktoolskr <zenmaster783@gmail.com>
2024-07-08 15:17:49 +0300

    Update mapbuilder.py

--- mapbuilder.py
+++ mapbuilder.py
@@ -1,44 +1,50 @@
 import pandas as pd
 from opencage.geocoder import OpenCageGeocode
 import folium
-from folium.plugins import MarkerCluster
-from folium.plugins import LocateControl
+from folium.plugins import MarkerCluster, LocateControl, Search
 import random
 
 # Load your CSV file and preprocess
 df = pd.read_csv('addresses.csv')
 
-# Initialize OpenCage geocoder with your API key
-key = '781b89ffc5eb47ab8e1b849822beacf1'
-geocoder = OpenCageGeocode(key)
+# Ensure Latitude and Longitude are numeric
+df['Latitude'] = pd.to_numeric(df['Latitude'], errors='coerce')
+df['Longitude'] = pd.to_numeric(df['Longitude'], errors='coerce')
+
+# Drop rows with invalid Latitude or Longitude values
+df = df.dropna(subset=['Latitude', 'Longitude'])
+print('finished working with df')
 
 def random_color():
     return random.choice(['lightgray', 'beige', 'darkblue', 'orange', 'red', 'pink', 'darkred', 'lightgreen', 'darkpurple', 'blue', 'lightred', 'black', 'darkgreen', 'cadetblue', 'gray', 'green', 'lightblue', 'purple', 'white'])
 
 
-def get_lat_long(address):
-    result = geocoder.geocode(address)
-    if result:
-        return result[0]['geometry']['lat'], result[0]['geometry']['lng']
-    return None, None
-
-# Apply the function to your DataFrame
-df['Latitude'], df['Longitude'] = zip(*df['address'].apply(get_lat_long))
-
-# Save the updated DataFrame to a new CSV (if needed)
-df.to_csv('updated_addresses.csv', index=False)
-
 # Create a map centered around an average location
 map_osm = folium.Map(location=[df['Latitude'].mean(), df['Longitude'].mean()], zoom_start=12)
+
+# Add a MarkerCluster layer
+marker_cluster = MarkerCluster().add_to(map_osm)
+
 
 # Add markers to the map
 for _, row in df.iterrows():
     c = random_color()
+    address = ' '.join(row['address'].split(' ')[1:])  # Remove the first word
+    popup_text = f"{address}, {row['code']}"
     folium.Marker(
         location=[row['Latitude'], row['Longitude']],
-        popup=f"{row['code']}",
+        popup=popup_text,
         icon=folium.Icon(color=c)
-    ).add_to(map_osm)
+    ).add_to(marker_cluster)
+
+# Add a search control to the map
+search = Search(
+    layer=marker_cluster,
+    geom_type='Point',
+    placeholder='Search for an address',
+    collapsed=False,
+    search_label='address'
+).add_to(map_osm)
 
 # Add a Leaflet control for locating user's position
 LocateControl().add_to(map_osm)
